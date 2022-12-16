11be9b7e3fdc9ea4e49844d974fb37d0c31445d2
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2022-12-16 22:49:40 +0100

    pe003

--- pe003/src/main.rs
+++ pe003/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    const NUM: u64 = 600851475143;
+    let answer = largest_prime_factor(NUM);
+    println!("{}", answer);
+}
+
+fn largest_prime_factor(mut n: u64) -> u64 {
+    let mut test = 2;
+    let mut largest = 0;
+    while n != 1 {
+       while n % test == 0 {
+           largest = test;
+           n = n / test;
+       } 
+       test += 1;
+    }
+    largest
 }
