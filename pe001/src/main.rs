c845b880b6e2c07511940f9a30cf157e7329eebd
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2022-12-16 22:34:06 +0100

    pe001

--- pe001/src/main.rs
+++ pe001/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = 0;
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            s += i;
+        }
+    }
+    println!("{s}");
 }
