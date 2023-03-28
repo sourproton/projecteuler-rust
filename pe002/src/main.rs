885c52610bcf09868fd74d93188f6b46a45d2622
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-03-28 18:57:21 +0200

    solution with traits and methods

--- pe002/src/main.rs
+++ pe002/src/main.rs
@@ -1,39 +1,58 @@
 use std::time::SystemTime;
 
-const LIMIT: isize = 4_000_000;
+const LIMIT: u32 = 4_000_000;
 
 fn main() {
     let time = SystemTime::now();
 
-    let answer = sum_even_fib(LIMIT);
+    let answer = Fib::sum_even_until(LIMIT);
 
     println!("answer: {answer}");
     println!("elapsed time: {} ms", time.elapsed().unwrap().as_millis());
 }
 
-fn update_fib(fib: &mut (isize, isize)) {
-    (fib.0, fib.1) = (fib.1, fib.0 + fib.1);
+/// Holds a Fibonacci pair
+struct Fib {
+    first: u32,
+    second: u32,
 }
 
-fn sum_even_fib(limit: isize) -> isize {
-    let mut fib = (1, 2);
-    let mut s = 0;
+impl Fib {
+    /// Stating from `Fib { 1, 2 }`, sums all the pair Fibonacci numbers up to `limit`
+    fn sum_even_until(limit: u32) -> u32 {
+        let mut fib = Fib::start();
+        let mut s = 0;
 
-    while fib.1 < limit {
-        if fib.1 % 2 == 0 {
-            s += fib.1;
+        while fib.second < limit {
+            if fib.second % 2 == 0 {
+                s += fib.second;
+            }
+            fib.update();
         }
-        update_fib(&mut fib);
+
+        s
     }
 
-    s
+    /// Returns the first Fibonacci pair (1, 2)
+    fn start() -> Self {
+        Fib {
+            first: 1,
+            second: 2,
+        }
+    }
+
+    /// Updated the Fibonacci pair to its next iteration on the sequence
+    fn update(&mut self) {
+        (self.first, self.second) = (self.second, self.first + self.second);
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::sum_even_fib;
+    use crate::Fib;
+
     #[test]
     fn test_sum_multiples() {
-        assert_eq!(sum_even_fib(89), 2 + 8 + 34);
+        assert_eq!(Fib::sum_even_until(89), 2 + 8 + 34);
     }
 }
